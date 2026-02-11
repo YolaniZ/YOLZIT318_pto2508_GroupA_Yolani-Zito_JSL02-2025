831b4622d93e8399f2111890114a6f794edc43a0
YolaniZ <yolanzito@gmail.com>
2026-02-11 15:00:31 +0200

    Updated Java script

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+const title1 = prompt ("Enter task 1 title");
+console.log (title1);
+const description1 = prompt ("Enter task 1 description");
+console.log (description1);
+const status1 = prompt ("Enter task 1 status");
+console.log (status1);
+
+const title2 = prompt("Enter task 2 title");
+console.log(title2);
+const description2 = prompt("Enter task 2 description");
+console.log(description2);
+const status2 = prompt("Enter task 2 status");
+console.log(status2);
+
+
